133e845f68591f786903f08b97669b94820f25e9
Adhvik Kanagala <52569612+addykan@users.noreply.github.com>
2020-08-20 11:54:36 -0400

    Update BungieGET.py

--- BungieGET.py
+++ BungieGET.py
@@ -5,3 +5,11 @@ Created on Thu Aug 20 11:47:07 2020
 @author: addyk
 """
 
+'''
+This code is used to pull data from Bungie's servers about what any player is doing in D2
+
+The expectation is to input a player ID (from Bungie) and to receive information in JSON(?) format about what said player is doing at the moment
+'''
+import requests
+import requests_oauthlib
+import json
